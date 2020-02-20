840f073d3efe9b47112a38bd36b9fa8a2d0db1bd
zhanglei <zhanglei@landicorp.com>
2020-02-20 16:22:09 +0800

    备份

--- customview/src/main/java/me/study/customview/SimpleView.java
+++ customview/src/main/java/me/study/customview/SimpleView.java
@@ -2,11 +2,16 @@ package me.study.customview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class SimpleView extends View {
+
+    private int width;
+    private int height;
+
     public SimpleView(Context context) {
         super(context);
     }
@@ -18,17 +23,25 @@ public class SimpleView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // 单位为px像素
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = MeasureSpec.getSize(heightMeasureSpec);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(width / 2, height / 2, width / 3, paint);
     }
 }
